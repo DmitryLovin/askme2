ead29bcb78bc5f8f556be09baf71d0d150a23f74
dmitrylovin <dmitrylovin@gmail.com>
2022-12-12 18:18:23 +0100

    nickname validation, email validation, question validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ApplicationRecord
   has_secure_password
 
   before_save :downcase_nickname
-  
-  validates :email, presence: true, uniqueness: true
+
+  validates :nickname,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 40 },
+            format: { with: /\A[0-9a-z_]+\z/i, message: "only allows digits, letters and underscore symbols (_)" }
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def downcase_nickname
     nickname.downcase!
