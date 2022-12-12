62831caac8716efd841f6ef6674ad308457936d2
dmitrylovin <dmitrylovin@gmail.com>
2022-12-12 17:58:22 +0100

    lesson 55

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,51 @@
 class UsersController < ApplicationController
-  def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
-    @user = User.create(user_params)
+  before_action :set_user, only: %i[edit update destroy]
 
-    session[:user_id] = @user.id
-    
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались'
+    else
+      flash.now[:alert] = "Вы неправильно заполнили поля формы регистрации"
+      render :new
+    end
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to root_path, notice: "Данные пользователя обновлены"
+    else
+      flash.now[:alert] = "При попытке сохранения пользователя возникла ошибка!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: "Пользователь удалён"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :nickname, :email, :password, :password_confirmation
+    )
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
