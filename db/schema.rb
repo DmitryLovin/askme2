2d4b693018a3d1a05868455cc7516ac47702088a
dmitrylovin <dmitrylovin@gmail.com>
2022-12-19 14:13:40 +0100

    indexing users by nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_181106) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_19_131240) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_181106) do
     t.string "password_digest"
     t.string "color"
     t.index ["email"], name: "index_users_on_email"
+    t.index ["nickname"], name: "index_users_on_nickname"
   end
 
 end
